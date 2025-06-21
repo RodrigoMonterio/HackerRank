1df3b0f9720d0616ffc3997e9997f798547cd203
RodrigoMonterio <19228050+RodrigoMonterio@users.noreply.github.com>
2025-06-20 21:46:22 -0300

    Solution Java 1D Array

---
+++ Java/src/main/java/com/monterio/datastructures/Java1DArray.java
@@ -0,0 +1,23 @@
+package com.monterio.datastructures;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Java1DArray {
+
+    public static void main(String[] args) {
+        var input = new Scanner(System.in);
+        var n = input.nextInt();
+        var numbers = new ArrayList<>();
+
+        for(var i = 0; i < n; i++) {
+            numbers.add(input.nextInt());
+        }
+
+        input.close();
+
+        for(var num : numbers){
+            System.out.println(num);
+        }
+    }
+}
