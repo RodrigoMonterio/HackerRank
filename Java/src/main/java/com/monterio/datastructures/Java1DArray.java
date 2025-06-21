89fb0588283736191fe68ac2f1dcdf1b829bf5fc
RodrigoMonterio <19228050+RodrigoMonterio@users.noreply.github.com>
2025-06-21 00:19:25 -0300

    Solution Java 1D Array v2

--- Java/src/main/java/com/monterio/datastructures/Java1DArray.java
+++ Java/src/main/java/com/monterio/datastructures/Java1DArray.java
@@ -1,24 +1,31 @@
 package com.monterio.datastructures;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 //https://www.hackerrank.com/challenges/java-1d-array-introduction/problem
 public class Java1DArray {
 
-    public static void main(String[] args) {
-        var input = new Scanner(System.in);
-        var n = input.nextInt();
-        var numbers = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        var bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        var n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        for(var i = 0; i < n; i++) {
-            numbers.add(input.nextInt());
+        List<String> str = new ArrayList<>();
+        for (var i = 0; i < n; i++) {
+            var line = bufferedReader.readLine();
+            if (line != null && !line.trim().isEmpty()) {
+                str.addAll(Arrays.asList(line.trim().split("\\s+")));
+            }
         }
 
-        input.close();
-
-        for(var num : numbers){
-            System.out.println(num);
-        }
+        str.stream().map(Integer::parseInt).forEach(System.out::println);
+        bufferedReader.close();
     }
 }
