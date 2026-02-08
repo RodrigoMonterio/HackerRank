81dc7543c4430d56c785e93378d1ac525a83a781
Rodrigo Monterio <rodrigomonterio@MacBook-Air-de-Rodrigo.local>
2026-02-08 16:36:10 -0300

    initial basic challege

---
+++ C++/HackerRank-Cpp/Introduction/helloworld/helloworld.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rodrigo Monterio on 08/02/26.
+//
+
+#include "helloworld.h"
---
+++ C++/HackerRank-Cpp/Introduction/helloworld/helloworld.h
@@ -0,0 +1,13 @@
+//
+// Created by Rodrigo Monterio on 08/02/26.
+//
+
+#ifndef C_HELLOWORLD_H
+#define C_HELLOWORLD_H
+
+
+class helloworld {
+};
+
+
+#endif //C_HELLOWORLD_H
